ba204394683f901173fe3800b3a6501f7f01c407
Darius Suplica <supdar@protonmail.com>
2018-06-17 21:57:31 -0700

    did stuff
    -Removed test.sh (only used for debugging)
    
    -improved formatting
    
    -added extra -- options
    
    -added option to boot TWRP before beginning backup
    
    - removed adbpath, replaced with  sdkpath variable
    
    - added delay option
    
    - will now ask for passphrase before copying
    
     - added a few functions

--- backup.sh
+++ backup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#TODO: no longer require a mounted drive, work with folder instead
 if [[ $EUID -ne 0 ]]; then
-   echo "Please run this script with sudo" 1>&2
+   echo "Please run this script with as root" 1>&2
    exit;
 fi
 if (! ls -a ~/.dsscripts/backup.sh > /dev/null) && (! ls -a ~/.dsscripts/backup.sh/TMP > /dev/null); then
@@ -11,50 +10,70 @@ if (! ls -a ~/.dsscripts/backup.sh > /dev/null) && (! ls -a ~/.dsscripts/backup.
 fi
 
 
-function initialize {
-  if [ "$1" == "bkpt" ]; then
-    read -p "Enter the path to disk or directory to back up to: " bkpt
-    echo $bkpt > ~/.dsscripts/backup.sh/bkpt
-  elif [ "$1" == "phonesrc" ]; then
-    read -p "Enter the path to the HOME directory on the phone: " phonesrc
-    echo $phonesrc > ~/.dsscripts/backup.sh/phonesrc
-  elif [ "$1" == "adbpath" ]; then
-    read -p "Enter the path to your copy of adb: " adbpath
-    echo $adbpath > ~/.dsscripts/backup.sh/adbpath
+function initializevars {
+  if [ "$1" == "BKPT" ]; then
+    read -p "Enter the path to disk or directory to back up to: " BKPT
+    echo $BKPT > ~/.dsscripts/backup.sh/BKPT
+  elif [ "$1" == "PHONESRC" ]; then
+    read -p "Enter the path to the HOME directory on the phone: " PHONESRC
+    echo $PHONESRC > ~/.dsscripts/backup.sh/PHONESRC
+  elif [ "$1" == "SDKPATH" ]; then
+    read -p "Enter the path to your copy of the sdk. It should include both adb and fastboot: " SDKPATH
+    echo $SDKPATH > ~/.dsscripts/backup.sh/SDKPATH
+  elif [ "$1" == "twrp" ]; then
+    read -p "Enter the path to your copy of TWRP: " TWRPPATH
+    echo $TWRPPATH > ~/.dsscripts/backup.sh/TWRPPATH
   else
     echo "option not recognized. Run with the option -h or --help to see "
   fi
   if [ $CONT ]; then
     return;
   else
-    exit;
   fi
 }
 
 function help {
+
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   echo "This script should be run using sudo"
   echo "Usage: backup.sh [- h,s] [options]"
-  echo " -h, --help: displays this text"
-  echo " -s: sets variables even if the already exist"
-  echo "with -s, type bkpt to set the directory to back up to, phonesrc to set the directory on the phone to back up from, and adbpath to set the path to your copy of adb"
-  echo "or with no arguments to set all 3"
+  echo " -h, --help: displays help text"
+  echo " -s, --set: sets variables even if the already exist"
+  echo "with -s or --set, type BKPT to set the directory to back up to, PHONESRC to set the directory on the phone to back up from, SDKPATH to set the path to your copy of the sdk, and TWRPPATH to set the path to your local copy of TWRP"
+  echo "or with no arguments to set all 4"
+  echo " -d, --delaydmg: sets a delay period before creating the disk image in minutes. Use this to prevent high CPU usage when the computer is in use."
+  echo "follow up with either 'm' or 'h' to select a duration in minutes or hours, respectively. Unless one of these is specified the time will default to seconds "
   exit;
-}
-if [ "$1" == "-h" ]; then
-  help;
-elif [ "$1" == "--help" ]; then
-  help;
 fi
 
-if [ "$1" == "-s" ]; then
+
+if [ "$1" == "-s" ] || [ "$1" == "--set"]; then
   if ["$2" > /dev/null ]; then
-    initialize "$2"
+    unset CONT
+    initializevars "$2"
   else
     CONT="1"
-    initialize bkpt
-    initialize phonesrc
+    initializevars BKPT
+    initializevars PHONESRC
+    initializevars TWRPPATH
     unset CONT
-    initialize adbpath
+    initializevars SDKPATH
+  fi
+  exit;
+fi
+
+if [ "$1" == "-d" ] || [ "$1" == "--delaydmg"]; then
+  if ["$2" == "m"]; then
+    DELAY=$((60*$3))
+  elif ["$2" == "h"]; then
+    DELAY=$((3600*$3))
+  elif [$2 -gt 0]; then
+    DELAY=$2
+  else
+    echo "Invalid input to delay time"
+    exit;
   fi
 fi
 
@@ -62,37 +81,71 @@ function checkfiles {
 if [ -f ~/.dsscripts/backup.sh/$1 ]; then
    return;
 else
-   initialize $1;
+   initializevars $1;
+    if [! $CONT ]; then
 fi
 }
 CONT="1"
-checkfiles "bkpt"
-checkfiles "phonesrc"
+checkfiles "BKPT"
+checkfiles "PHONESRC"
+checkfiles "TWRPPATH"
 unset CONT
-checkfiles "adbpath"
-bkpt=$(cat ~/.dsscripts/backup.sh/bkpt)
-phonesrc=$(cat ~/.dsscripts/backup.sh/phonesrc)
-adbpath=$(cat ~/.dsscripts/backup.sh/adbpath)
+checkfiles "SDKPATH"
+BKPT=$(cat ~/.dsscripts/backup.sh/BKPT)
+PHONESRC=$(cat ~/.dsscripts/backup.sh/PHONESRC)
+TWRPPATH=$(cat ~/.dsscripts/backup.sh/TWRPPATH)
+SDKPATH=$(cat ~/.dsscripts/backup.sh/SDKPATH)
 
+function pause {
+  read -p "Press enter to continue"
+}
 
+function checkplugged {
+  if (! $SDKPATH/adb devices > /dev/null); then
+    echo "Please ensure the phone is plugged in and usb debugging is enabled"
+    pause
+    checkplugged
+  else
+    return
+  fi
+}
+
+function getdmgpassphrase {
+  read -s -p "Enter disk image passphrase" DMGPASSPHRASE1
+  read -s -p "Enter again" DMGPASSPHRASE2
+  if [ $DMGPASSPHRASE1 == $DMGPASSPHRASE2 ]; then
+    DMGPASSPHRASE=$DMGPASSPHRASE1
+    return
+  else
+    echo "Passphrases do not match"
+    getdmgpassphrase
+  fi
+}
 if (! ls -a ~/.dsscripts/backup.sh/TMP > /dev/null); then
     mkdir ~/.dsscripts/backup.sh/TMP
 fi
-if (ls $bkpt > /dev/null); then
-  if (! $adbpath devices > /dev/null); then
-    echo "Please plug in ONE phone and ensure adb is enabled"
-    exit;
+if (ls $BKPT > /dev/null); then
+  if [! $NOTWRP ]; then
+    if (! $SDKPATH/fastboot devices > /dev/null); then
+      echo "Please plug in ONE phone booted to the bootloader (The bootloader should be unlocked)"
+      exit;
+    fi
+    $SDKPATH/fastboot boot $TWRPPATH
+    echo "You may now unplug the phone. Once you have completed performing a local backup, plug it back in. Once you are done,"
+    pause
   fi
+  checkplugged
+  #!!!:  THIS MIGHT BE A HUGE VULNERABILITY
+  getdmgpassphrase
   sudo rm -rf ~/.dsscripts/backup.sh/TMP/*
   echo "Beginning to copy files, this will take a while"
-  sudo $adbpath pull $phonesrc ~/.dsscripts/backup.sh/TMP
+  sudo $SDKPATH/adb pull $PHONESRC ~/.dsscripts/backup.sh/TMP
   echo "Files copied, it is now safe to unplug the phone."
   TODAY=`date '+%Y-%m-%d'`;
-  sudo hdiutil create -encryption -stdinpass -srcfolder ~/.dsscripts/backup.sh/TMP/* ~/.dsscripts/backup.sh/TMP/$TODAY.dmg
-  SRC="~/.dsscripts/backup.sh/TMP/$TODAY.dmg"
-  DESTINATION="$bkpt/$TODAY.dmg"
-  sudo touch $DESTINATION
-  sudo mv ~/.dsscripts/backup.sh/TMP/$TODAY.dmg $bkpt/$TODAY.dmg
+  sleep $DELAY
+  printf '%s' $DMGPASSPHRASE | hdiutil create -encryption -stdinpass -volname $TODAY -srcfolder ~/.dsscripts/backup.sh/TMP/* ~/.dsscripts/backup.sh/TMP/$TODAY.dmg
+  sudo touch $BKPT/$TODAY.dmg
+  sudo mv ~/.dsscripts/backup.sh/TMP/$TODAY.dmg $BKPT/$TODAY.dmg
   echo "Backup finished."
 else
   echo "Please ensure the backup folder exists. If you are backing up to a drive, ensure it is mounted."
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bkpt="/Volumes/Darius"
-if (~/android/adb devices &> /dev/null); then
-  echo "1"
-else
-  echo "0"
-fi
