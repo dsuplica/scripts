5dbd26239811572ae47d1cbc4a85ad24745b2f65
Darius Suplica <supdar@protonmail.com>
2018-07-03 15:57:22 -0400

    Comments & fixes
    - added comments
    - moved the root check to the part where it's actually relevant
    - fixed the twrp booting issue
    - removed redundancies

--- backup.sh
+++ backup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "Please run this script with as root" 1>&2
-   exit;
+#Help function
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  echo "This script should be run using sudo"
+  echo "Usage: backup.sh [- h,s] [options]"
+  echo " -h, --help: displays help text"
+  echo " -s, --set: sets variables even if the already exist"
+  echo "with -s or --set, type BKPT to set the directory to back up to, PHONESRC to set the directory on the phone to back up from, SDKPATH to set the path to your copy of the sdk, and TWRPPATH to set the path to your local copy of TWRP"
+  echo "or with no arguments to set all 4"
+  echo " -d, --delaydmg: sets a delay period before creating the disk image in minutes. Use this to prevent high CPU usage when the computer is in use."
+  echo "follow up with either 'm' or 'h' to select a duration in minutes or hours, respectively. Unless one of these is specified the time will default to seconds "
+  exit;
 fi
+# directory check
 if (! ls -a ~/.dsscripts/backup.sh > /dev/null) && (! ls -a ~/.dsscripts/backup.sh/TMP > /dev/null); then
   mkdir ~/.dsscripts/
   mkdir ~/.dsscripts/backup.sh/
   mkdir ~/.dsscripts/backup.sh/TMP
 fi
-
-
+# asks for variable input and sets set variable
 function initializevars {
   if [ "$1" == "BKPT" ]; then
     read -p "Enter the path to disk or directory to back up to: " BKPT
@@ -33,20 +41,7 @@ function initializevars {
   fi
 }
 
-
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-  echo "This script should be run using sudo"
-  echo "Usage: backup.sh [- h,s] [options]"
-  echo " -h, --help: displays help text"
-  echo " -s, --set: sets variables even if the already exist"
-  echo "with -s or --set, type BKPT to set the directory to back up to, PHONESRC to set the directory on the phone to back up from, SDKPATH to set the path to your copy of the sdk, and TWRPPATH to set the path to your local copy of TWRP"
-  echo "or with no arguments to set all 4"
-  echo " -d, --delaydmg: sets a delay period before creating the disk image in minutes. Use this to prevent high CPU usage when the computer is in use."
-  echo "follow up with either 'm' or 'h' to select a duration in minutes or hours, respectively. Unless one of these is specified the time will default to seconds "
-  exit;
-fi
-
-
+# checks if intent to set variables
 if [ "$1" == "-s" ] || [ "$1" == "--set" ]; then
   if [ "$2" > /dev/null ]; then
     unset CONT
@@ -62,6 +57,13 @@ if [ "$1" == "-s" ] || [ "$1" == "--set" ]; then
   exit;
 fi
 
+#Stuff that'll involve the actual running of the script goes after here - root check
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run this script with as root" 1>&2
+   exit;
+fi
+
+#Checks for dmg delay
 if [ "$1" == "-d" ] || [ "$1" == "--delaydmg" ]; then
   if ["$2" == "m"]; then
     DELAY=$((60*$3))
@@ -75,6 +77,7 @@ if [ "$1" == "-d" ] || [ "$1" == "--delaydmg" ]; then
   fi
 fi
 
+# function ensures the file for the designated variable exists. If it doesn't, it'll prompt for it
 function checkfiles {
 if [ -f ~/.dsscripts/backup.sh/$1 ]; then
    return;
@@ -87,6 +90,8 @@ else
     fi
   fi
 }
+
+#Checks variables are saved and loads them
 CONT="1"
 checkfiles "BKPT"
 checkfiles "PHONESRC"
@@ -98,10 +103,12 @@ PHONESRC=$(cat ~/.dsscripts/backup.sh/PHONESRC)
 TWRPPATH=$(cat ~/.dsscripts/backup.sh/TWRPPATH)
 SDKPATH=$(cat ~/.dsscripts/backup.sh/SDKPATH)
 
+#Stops the script until the user prompts to continue
 function pause {
   read -p "Press enter to continue"
 }
 
+#ensures the device is plugged in and adb is enabled
 function checkplugged {
   if (! $SDKPATH/adb devices > /dev/null); then
     echo "Please ensure the phone is plugged in and usb debugging is enabled"
@@ -111,6 +118,7 @@ function checkplugged {
     return
   fi
 }
+# gets passphrase to use for the dmg later and verifies it
 unset CONT
 function getpasswd {
   while [[ ! $CONT ]]; do
@@ -125,14 +133,11 @@ function getpasswd {
 }
 
 #DEBUG
-NOTWRP="0"
+NOTWRP="1"
 DELAY="0"
 #/DEBUG
-if (! ls -a ~/.dsscripts/backup.sh/TMP > /dev/null); then
-    mkdir ~/.dsscripts/backup.sh/TMP
-fi
-if (ls $BKPT > /dev/null); then
-  if [ ! $NOTWRP ]; then
+if (ls $BKPT > /dev/null); then #ensures the backup point exists
+  if [ "$NOTWRP" ]; then #checks to see if it should boot from an image
     if (! $SDKPATH/fastboot devices > /dev/null); then
       echo "Please plug in ONE phone booted to the bootloader (The bootloader should be unlocked)"
       exit;
@@ -145,13 +150,13 @@ if (ls $BKPT > /dev/null); then
   #!!!:  THIS MIGHT BE A HUGE VULNERABILITY
   getpasswd
   sudo rm -rf ~/.dsscripts/backup.sh/TMP/*
-  echo " "
+  echo " " #newline
   echo "Beginning to copy files, this will take a while"
-  sudo $SDKPATH/adb pull $PHONESRC ~/.dsscripts/backup.sh/TMP
+  sudo $SDKPATH/adb pull $PHONESRC ~/.dsscripts/backup.sh/TMP #copies files to a tmp dir not used by anything else
   echo "Files copied, it is now safe to unplug the phone."
   TODAY=`date '+%Y-%m-%d'`;
   sleep $DELAY
-  printf '%s' $DMGPASSPHRASE | hdiutil create -encryption -stdinpass -volname $TODAY -srcfolder ~/.dsscripts/backup.sh/TMP/* ~/.dsscripts/backup.sh/TMP/$TODAY.dmg
+  printf '%s' $DMGPASSPHRASE | hdiutil create -encryption -stdinpass -volname $TODAY -srcfolder ~/.dsscripts/backup.sh/TMP/* ~/.dsscripts/backup.sh/TMP/$TODAY.dmg #passes the passphrase as a dmg password
   sudo touch $BKPT/$TODAY.dmg
   sudo mv ~/.dsscripts/backup.sh/TMP/$TODAY.dmg $BKPT/$TODAY.dmg
   echo "Backup finished."
